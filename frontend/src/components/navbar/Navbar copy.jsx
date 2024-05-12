0b0b0f1cad6f6aca2374b17e5731e1397d86a42f
Arturitooo <arturito.python@gmail.com>
2024-05-12 18:36:42 +0200

    Change navbar component to have icons

--- frontend/src/components/navbar/Navbar copy.jsx
+++ frontend/src/components/navbar/Navbar copy.jsx
@@ -1,25 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { styled, useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import MuiDrawer from '@mui/material/Drawer';
+import Drawer from '@mui/material/Drawer';
+import CssBaseline from '@mui/material/CssBaseline';
 import MuiAppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-//icons
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 import GroupIcon from '@mui/icons-material/Group';
 import DataSaverOffIcon from '@mui/icons-material/DataSaverOff';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 import { useNavigate, Link, useLocation } from 'react-router-dom';
 import { AccountMenuNavbar } from './AccountMenuNavbar';
 import './Navbar.css'
@@ -34,110 +31,68 @@ const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
+    marginLeft: `-${drawerWidth}px`,
     ...(open && {
       transition: theme.transitions.create('margin', {
         easing: theme.transitions.easing.easeOut,
         duration: theme.transitions.duration.enteringScreen,
       }),
+      marginLeft: 0,
     }),
   }),
   
 );
 
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  backgroundColor:'#1D212F',
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: 'hidden',
-});
-
-const closedMixin = (theme) => ({
-  backgroundColor:'#1D212F',
-  transition: theme.transitions.create('width', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  
-  overflowX: 'hidden',
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up('sm')]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-end',
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-
 const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
   }),
 }));
 
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-  ({ theme, open }) => ({
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    boxSizing: 'border-box',
-    ...(open && {
-      ...openedMixin(theme),
-      '& .MuiDrawer-paper': openedMixin(theme),
-    }),
-    ...(!open && {
-      ...closedMixin(theme),
-      '& .MuiDrawer-paper': closedMixin(theme),
-    }),
-  }),
-);
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
 
 export function Navbar(props) {
+  //what's shown in the sidebar
   const {content} = props
   const location = useLocation()
   const path = location.pathname
   const navigate = useNavigate()
+
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   const [authenticated, setAuthenticated] = React.useState(false);
-  
-  const checkAuthenticationStatus = () => {
-    const token = localStorage.getItem('Token');
-    return token; 
-  };
 
-  useEffect(() => {
-    const isAuthenticated = checkAuthenticationStatus();
-    setAuthenticated(isAuthenticated);
-  }, []);
+const checkAuthenticationStatus = () => {
+  const token = localStorage.getItem('Token');
+  return token; 
+};
 
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+useEffect(() => {
+  const isAuthenticated = checkAuthenticationStatus();
+  setAuthenticated(isAuthenticated);
+}, []);
 
-  const handleDrawerClose = () => {
-    setOpen(false);
+  const handleDrawerToggle = () => {
+    setOpen(!open);
   };
 
   return (
@@ -145,28 +100,39 @@ export function Navbar(props) {
       <CssBaseline />
       <AppBar position="fixed" open={open} sx={{backgroundColor:'#fff', boxShadow: '0px 1px 0px 0px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(0,0,0,0.1),0px 1px 0px 0px rgba(0,0,0,0.1)'}}>
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between'}}>
-          {authenticated ? (
+        {authenticated ? (
           <IconButton
-            color="#1D212F"
-            aria-label="open drawer"
-            
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            { open ? ( <ChevronLeftIcon />) : (<MenuIcon />) }
-          </IconButton>
-          ) : (null)}
-          <Typography variant="h6" noWrap component="div">
+          color="#1D212F"
+          aria-label="open drawer"
+          onClick={handleDrawerToggle}
+          edge="start"
+          sx={{ mr: 2 }}
+        >
+          { open ? ( <ChevronLeftIcon />) : (<MenuIcon />) }  
+        </IconButton>
+        ) : (null)}
+          <Typography variant="h6" noWrap component="div" sx={{color:'#1D212F'}}>
           </Typography>
-          <AccountMenuNavbar/>
+            <AccountMenuNavbar/>
+          
         </Toolbar>
       </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader sx={{backgroundColor:'#1D212F',}}>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            backgroundColor: '#1D212F',
+            color: 'rgba(245, 247, 249, 0.7)',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+
         <Box sx={{ marginLeft:'10px', marginTop: '10px' }}>
         <a href='/'><img 
           src="..\src\assets\smtials_logo.png" 
@@ -174,11 +140,6 @@ export function Navbar(props) {
           width={"60%"}
         /></a>
         </Box>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon sx={{color:'white'}}/> : <ChevronLeftIcon sx={{color:'white'}}/>}
-          </IconButton>
-          
-        </DrawerHeader>
         <Divider />
         <List>
             {/* <ListItem key={1} disablePadding>
@@ -192,7 +153,7 @@ export function Navbar(props) {
                   style={{ 
                     position: 'relative',
                     fontSize:'large',
-                    marginRight: '12px', 
+                    marginRight: '8px', 
                   }} 
                 />
                 <ListItemText sx={{color: '#F5F7F9'}} primary={"Notes"} />
@@ -204,7 +165,7 @@ export function Navbar(props) {
                   style={{ 
                     position: 'relative',
                     fontSize:'large',
-                    marginRight: '12px', 
+                    marginRight: '8px', 
                   }} 
                 />
                 <ListItemText sx={{color: '#F5F7F9'}} primary={"Team"} />
@@ -216,7 +177,7 @@ export function Navbar(props) {
                   style={{ 
                     position: 'relative',
                     fontSize:'large',
-                    marginRight: '12px', 
+                    marginRight: '8px', 
                   }} 
                 />
                 <ListItemText sx={{color: '#F5F7F9'}} primary={"Monitoring"} />
@@ -228,7 +189,8 @@ export function Navbar(props) {
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
-            {content}      
+            {content}
+       
       </Main>
     </Box>
   );
