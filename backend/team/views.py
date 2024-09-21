d737711693243a238b4db4a89b3b3b1a9c97a468
Artur Hirsch <arturito.python@gmail.com>
2024-09-21 16:37:57 +0200

    GitLab API collecting for the Commits and MRs comments

--- backend/team/views.py
+++ backend/team/views.py
@@ -180,11 +180,11 @@ class TeamMemberGitIntegrationDataCreateAPIView(CreateAPIView):
         # get needed data to the variables
         groupID = data.get("teammemberGitGroupID")
         userID = data.get("teammemberGitUserID")
-        acccessToken = data.get("teammemberGitPersonalAccessToken")
+        accessToken = data.get("teammemberGitPersonalAccessToken")
 
         # provide api needed info
         url = f"https://gitlab.com/api/v4/groups/{groupID}/members"
-        headers = {"PRIVATE-TOKEN": acccessToken, "Content-Type": "application/json"}
+        headers = {"PRIVATE-TOKEN": accessToken, "Content-Type": "application/json"}
 
         # make API call
         try:
@@ -288,32 +288,39 @@ class TeammemberCodingStatsCreateAPIView(CreateAPIView):
         gitIntegrationData = TeamMemberGitIntegrationData.objects.filter(
             teammember=teammember
         )
-        limesDate = str(datetime.today() - timedelta(days=30))
-        dt_object = datetime.strptime(limesDate, "%Y-%m-%d %H:%M:%S.%f")
-        # Convert to the desired ISO 8601 format (without microseconds and with UTC 'Z' suffix)
-        limesDate_iso_format = dt_object.strftime("%Y-%m-%dT%H:%M:%SZ")
-        apiCallsInput = gitIntegrationData
-        # Add the 'limesDate_iso_format' to the dictionary
-        apiCallsInput["limesDate"] = limesDate_iso_format
 
-        # TODO make created mrs api call
+        # set date limes to 30 days ago and convert to needed format
+        data_limitation = str(datetime.today() - timedelta(days=30))
+        dt_object = datetime.strptime(data_limitation, "%Y-%m-%d %H:%M:%S.%f")
+        data_limitation_iso_format = dt_object.strftime("%Y-%m-%dT%H:%M:%SZ")
+        apiCallsInput = gitIntegrationData
+        # Add the 'data_limitation_iso_format' to the dictionary to use in api calls
+        apiCallsInput["data_limitation"] = data_limitation_iso_format
+
+        # TODO make created mrs api call with gitlab_merge_requests_api_call
         # apiCallsInput["requestType"] = "author_id"
-        # TODO make reviewed mrs api call
+        # TODO make reviewed mrs api call with gitlab_merge_requests_api_call
         # apiCallsInput["requestType"] = "reviewer_id"
-        # TODO make projects api call
+        # TODO make projects api call with gitlab_project_api_call
         # apiCallsInput["projects_list"] = [...]
+        # TODO make commits created api call with gitlab_commits_created_api_call
+        # apiCallsInput["projects_list"] = [...]
+        # TODO make commits difference api call with gitlab_commits_diff_api_call
+        # apiCallsInput["commits_list"] = created_commits_data_dict
+        # TODO make MRs comments api call with gitlab_commits_diff_api_call
+        # apiCallsInput["mrs_list"] = mr_data_dict
 
     def gitlab_merge_requests_api_call(self, data):
-        # TODO to make the api call you need to provide if check author_id or reviewer_id
+        # To make the api call you need to provide if check author_id or reviewer_id
         # get needed data to the variables
         requestType = data.get("requestType")
         userID = data.get("teammemberGitUserID")
-        acccessToken = data.get("teammemberGitPersonalAccessToken")
-        dataLimes = data.get("limesDate")
+        accessToken = data.get("teammemberGitPersonalAccessToken")
+        data_limitation = data.get("data_limitation")
 
         # provide api needed info
-        url = f"https://gitlab.com/api/v4/merge_requests?{requestType}={userID}&created_after={dataLimes}"
-        headers = {"PRIVATE-TOKEN": acccessToken, "Content-Type": "application/json"}
+        url = f"https://gitlab.com/api/v4/merge_requests?{requestType}={userID}&created_after={data_limitation}"
+        headers = {"PRIVATE-TOKEN": accessToken, "Content-Type": "application/json"}
 
         # make API call with basic error handling
         try:
@@ -327,6 +334,7 @@ class TeammemberCodingStatsCreateAPIView(CreateAPIView):
                     mr_id = record["id"]
                     mr_data_dict[mr_id] = {
                         "project_id": record["project_id"],
+                        "iid": record["iid"],
                         "created_at": record["created_at"],
                         "merged_at": record["merged_at"],
                     }
@@ -339,18 +347,19 @@ class TeammemberCodingStatsCreateAPIView(CreateAPIView):
             return {"success": False, "message": str(e)}
 
     def gitlab_project_api_call(self, data):
-        # TODO to make the api call you need to provide projects_list
+        # To make the api call you need to provide projects_list
         # get needed data to the variables
         projects_list = data.get("projects_list")
-        acccessToken = data.get("teammemberGitPersonalAccessToken")
+        accessToken = data.get("teammemberGitPersonalAccessToken")
+        data_limitation = data.get("data_limitation")
 
         project_data_dict = {}
 
         for project in projects_list:
             # provide api needed info
-            url = f"https://gitlab.com/api/v4/projects/{project}"
+            url = f"https://gitlab.com/api/v4/projects/{project}&created_after={data_limitation}"
             headers = {
-                "PRIVATE-TOKEN": acccessToken,
+                "PRIVATE-TOKEN": accessToken,
                 "Content-Type": "application/json",
             }
 
@@ -359,12 +368,10 @@ class TeammemberCodingStatsCreateAPIView(CreateAPIView):
                 response = requests.get(url, headers=headers)
                 if response.status_code >= 200 and response.status_code < 300:
                     data = response.json()
-                    # Create a dictionary to store the MR data with MR ID as the key
-                    mr_data_dict = {}
                     # Get the needed data
                     for record in data:
                         project_id = project
-                        mr_data_dict[project_id] = {
+                        project_data_dict[project_id] = {
                             "project_name": record["name"],
                             "project_url": record["web_url"],
                         }
@@ -374,6 +381,153 @@ class TeammemberCodingStatsCreateAPIView(CreateAPIView):
                 # Handle any errors that occur during the request
                 return {"success": False, "message": str(e)}
         return project_data_dict
+
+    def gitlab_commits_created_api_call(self, data):
+        # To make the api call you need to provide projects_list
+        # get needed data to the variables
+        projects_list = data.get("projects_list")
+        userID = data.get("teammemberGitUserID")
+        accessToken = data.get("teammemberGitPersonalAccessToken")
+        data_limitation = data.get("data_limitation")
+
+        created_commits_data_dict = {}
+
+        for project in projects_list:
+            # provide api needed info
+            url = f"https://gitlab.com/api/v4/projects/{project}/repository/commits?author_id={userID}&created_after={data_limitation}"
+            headers = {
+                "PRIVATE-TOKEN": accessToken,
+                "Content-Type": "application/json",
+            }
+
+            # make API call with basic error handling
+            try:
+                response = requests.get(url, headers=headers)
+                if response.status_code >= 200 and response.status_code < 300:
+                    data = response.json()
+                    # Initialize the list of commits for the project if not already present
+                    if project not in created_commits_data_dict:
+                        created_commits_data_dict[project] = []
+                    # Get the needed data for each commit
+                    for record in data:
+                        commit_info = {
+                            "commit_short_id": record["short_id"],
+                            "created_at": record["created_at"],
+                            "commit_web_url": record["web_url"],
+                        }
+                        # Append the commit data to the project's list of commits
+                        created_commits_data_dict[project].append(commit_info)
+                else:
+                    return False
+            except requests.exceptions.RequestException as e:
+                # Handle any errors that occur during the request
+                return {"success": False, "message": str(e)}
+        return created_commits_data_dict
+
+    def gitlab_commits_diff_api_call(self, data):
+        # To make the api call you need to provide projects_list
+        # get needed data to the variables
+        created_commits_data_dict = data
+        accessToken = data.get("teammemberGitPersonalAccessToken")
+        data_limitation = data.get("data_limitation")
+
+        commits_diff_data_dict = {}
+
+        for project_id, commits_list in created_commits_data_dict.items():
+            for commit in commits_list:
+                commit_short_id = commit["commit_short_id"]
+                # provide api needed info
+                url = f"https://gitlab.com/api/v4/projects/{project_id}/repository/{commit_short_id}/diff&created_after={data_limitation}"
+                headers = {
+                    "PRIVATE-TOKEN": accessToken,
+                    "Content-Type": "application/json",
+                }
+
+                # make API call with basic error handling
+                try:
+                    response = requests.get(url, headers=headers)
+                    if response.status_code >= 200 and response.status_code < 300:
+                        data = response.json()
+                        # Variables to track lines added and removed
+                        lines_added = 0
+                        lines_removed = 0
+                        added_lines_content = []
+                        removed_lines_content = []
+                        # Get the needed data for each commit
+                        for file_diff in data:
+                            diff_text = file_diff["diff"]
+                            # Split the diff into lines and count added and removed lines
+                            for line in diff_text.splitlines():
+                                if line.startswith("+") and not line.startswith("+++"):
+                                    lines_added += 1
+                                    added_lines_content.append(line[1:].strip())
+                                elif line.startswith("-") and not line.startswith(
+                                    "---"
+                                ):
+                                    lines_removed += 1
+                                    removed_lines_content.append(line[1:].strip())
+
+                        commit_diff_info = {
+                            "lines_added": lines_added,
+                            "lines_removed": lines_removed,
+                            "added_lines_content": added_lines_content,
+                            "removed_lines_content": removed_lines_content,
+                        }
+
+                        # Append the diff data to the project's commits
+                        if project_id not in commits_diff_data_dict:
+                            commits_diff_data_dict[project_id] = []
+
+                        commits_diff_data_dict[project_id].append(
+                            {
+                                "commit_short_id": commit_short_id,
+                                "diff_data": commit_diff_info,
+                            }
+                        )
+
+                    else:
+                        return f"Failed to fetch diff for commit {commit_short_id} in project {project_id}"
+                except requests.exceptions.RequestException as e:
+                    # Handle any errors that occur during the request
+                    return {"success": False, "message": str(e)}
+        return commits_diff_data_dict
+
+    def gitlab_mrs_comments_api_call(self, data):
+        # To make the api call you need to provide mrs_list
+        # get needed data to the variables
+        mr_data_dict = data.get("mr_data_dict")
+        accessToken = data.get("teammemberGitPersonalAccessToken")
+        data_limitation = data.get("data_limitation")
+        mrs_comments_data_dict = {}
+
+        for mr_id, mr in mr_data_dict.items():
+            mr_iid = mr["iid"]
+            project_id = mr["project_id"]
+
+            # provide api needed info
+            url = f"https://gitlab.com/api/v4/projects/{project_id}/merge_requests/{mr_iid}/notes?created_after={data_limitation}"
+            headers = {
+                "PRIVATE-TOKEN": accessToken,
+                "Content-Type": "application/json",
+            }
+
+            # make API call with basic error handling
+            try:
+                response = requests.get(url, headers=headers)
+                if response.status_code >= 200 and response.status_code < 300:
+                    data = response.json()
+                    # Get the needed data for each commit
+                    for record in data:
+                        mrs_comments_data_dict[mr_id] = {
+                            "id": record["id"],
+                            "body": record["body"],
+                        }
+                else:
+                    return False
+            except requests.exceptions.RequestException as e:
+                # Handle any errors that occur during the rMRequest
+                return {"success": False, "message": str(e)}
+        return mrs_comments_data_dict
 
 
 class TeammemberCodingStatsUpdateAPIView(UpdateAPIView):
